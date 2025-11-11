f94bf37f9d7252bee0324276d6ee1f1049543c64
Zlata Schastlivaya <s4astlivaya.zlata11@yandex.ru>
2025-11-11 16:56:13 +0300

    feat: solve task sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,9 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const longest = arr1.length >= arr2.length ? arr1 : arr2;
+  return longest.map((_, index) => (arr1[index] || 0) + (arr2[index] || 0));
 }
 
 /**
