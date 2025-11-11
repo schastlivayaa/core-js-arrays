2a3f60549aa1d2c6d92e542cb41ac87f15bffaef
Zlata Schastlivaya <s4astlivaya.zlata11@yandex.ru>
2025-11-11 19:35:58 +0300

    feat: solve task isValueEqualsIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -150,8 +150,8 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((elem, index) => elem === index);
 }
 
 /**
